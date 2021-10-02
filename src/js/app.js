023032a499ba70b012ebb5c17ac292b9ee908707
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2021-10-02 22:16:02 +0500

    swiper js
    banner slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,30 @@
-import Alpine from 'alpinejs'
- 
-window.Alpine = Alpine
- 
-Alpine.start()
+import Alpine from 'alpinejs';
 
-// alert()
+window.Alpine = Alpine;
+
+Alpine.start();
+
+// core version + navigation, pagination modules:
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// configure Swiper to use modules
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
