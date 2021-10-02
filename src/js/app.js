e498b71069df115f8a58cb7ec8955d9f12b07db2
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2021-10-03 00:24:45 +0500

    swiper
    added Autoplay module

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ window.Alpine = Alpine;
 Alpine.start();
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination, Scrollbar, Lazy } from 'swiper';
+import Swiper, { Navigation, Pagination, Scrollbar, Lazy, Autoplay } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -14,50 +14,47 @@ import 'swiper/css/scrollbar';
 import 'swiper/css/lazy';
 
 // configure Swiper to use modules
-Swiper.use([Navigation, Pagination, Scrollbar, Lazy]);
+Swiper.use([Navigation, Pagination, Scrollbar, Lazy, Autoplay]);
 
 const banner = new Swiper('.banner-slider', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-  // preloadImages: false,
-  // Enable lazy loading
-  lazy: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+	loop: true,
+	autoplay: {
+		delay: 5000,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true,
+	},
+ lazy: true,
+ pagination: {
+	el: '.swiper-pagination',
+	clickable: true,
+},
+navigation: {
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev',
+},
 });
 
 const partners = new Swiper('.partners-slider', {
-  // slidesPerView: 2,
-  // spaceBetween: 10,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 3,
-      spaceBetween: 10
-    },
-    // when window width is >= 640px
-    586: {
-      slidesPerView: 4,
-      spaceBetween: 10
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 10
-    },
-    1024: {
-      slidesPerView: 7,
-      spaceBetween: 10
-    },
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+	breakpoints: {
+		320: {
+			slidesPerView: 3,
+			spaceBetween: 10
+		},
+		586: {
+			slidesPerView: 4,
+			spaceBetween: 10
+		},
+		768: {
+			slidesPerView: 5,
+			spaceBetween: 10
+		},
+		1024: {
+			slidesPerView: 7,
+			spaceBetween: 10
+		},
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
 });
