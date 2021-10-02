a448cf3b8c924b2f0f0c2b8093f9f74d31f55581
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2021-10-02 23:24:23 +0500

    banner slider
    lazy load
    mobile slides

--- src/js/app.js
+++ src/js/app.js
@@ -5,24 +5,28 @@ window.Alpine = Alpine;
 Alpine.start();
 
 // core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination, Scrollbar, Lazy } from 'swiper';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/lazy';
 
 // configure Swiper to use modules
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination, Scrollbar, Lazy]);
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.banner-slider', {
   // Optional parameters
   // direction: 'vertical',
   loop: true,
-  // If we need pagination
+  // preloadImages: false,
+  // Enable lazy loading
+  lazy: true,
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
-  // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
