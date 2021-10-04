fc4b07730e2abb002d3ed74bdabdbdd25a0c4e7e
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2021-10-04 16:36:25 +0500

    modals logic

--- webpack.mix.js
+++ webpack.mix.js
@@ -37,20 +37,25 @@ mix
 	.js('src/js/app.js', 'js/scripts.js')
 	.sass('src/sass/main.sass', 'css/styles.css')
 	.tailwind()
-	.setPublicPath('dist/')
-	.serve('php -S 127.0.0.1:8080 -t ./', {
-		verbose: true,
-		watch: true,
-		dev: true,
-	});
+	.setPublicPath('dist/');
+	// .serve('php -S 127.0.0.1:8080 -t ./', {
+	// 	verbose: true,
+	// 	watch: true,
+	// 	dev: true,
+	// });
 
 if (mix.inProduction()) {
 	mix.version();
 } else {
 	mix.sourceMaps().webpackConfig({ devtool: 'inline-source-map' });
 	mix.browserSync({
-		proxy: '127.0.0.1:8080',
-		files: ['**.html', '**.php', 'dist/css/styles.css', 'dist/js/scripts.js']
+		// proxy: '127.0.0.1:8080',
+		// files: ['**.html', '**.php', 'dist/css/styles.css', 'dist/js/scripts.js']
+		server: {
+			baseDir: "./",
+		},
+		files: ['index.html', 'dist/css/styles.css', 'dist/js/scripts.js'],
+		notify: false
 	});
 }
 
