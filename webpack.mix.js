1eadec4d5934e1538a2ad22a41e61e4b5a3ddbab
Alexander Sabanov <alexsab@mail.ru>
2022-02-17 08:40:24 +0400

    upd webpack to work with php

--- webpack.mix.js
+++ webpack.mix.js
@@ -38,23 +38,21 @@ mix
 	.sass('src/sass/main.sass', 'css/styles.css')
 	.tailwind()
 	// .setPublicPath('dist/');
-	// .serve('php -S 127.0.0.1:8080 -t ./', {
-	// 	verbose: true,
-	// 	watch: true,
-	// 	dev: true,
-	// });
+	.serve('php -S 127.0.0.1:8080 -t ./', {
+		verbose: true,
+		watch: true,
+		dev: true,
+	});
 
 if (mix.inProduction()) {
 	mix.version();
 } else {
 	mix.sourceMaps().webpackConfig({ devtool: 'inline-source-map' });
 	mix.browserSync({
-		// proxy: '127.0.0.1:8080',
-		// files: ['**.html', '**.php', 'dist/css/styles.css', 'dist/js/scripts.js']
-		server: {
-			baseDir: "./",
-		},
-		files: ['index.html', 'css/styles.css', 'js/scripts.js'],
+		proxy: '127.0.0.1:8080',
+		files: ['**.html', '**.php', 'css/styles.css', 'js/scripts.js'],
+		// server: { baseDir: "./", },
+		// files: ['index.html', 'css/styles.css', 'js/scripts.js'],
 		notify: false
 	});
 }
